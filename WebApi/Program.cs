f0ae87fa839e78d20f78822a1dc05330c5e7209b
ibrahim <ibrahimklcprlr@gmail.com>
2024-01-14 17:15:53 +0300

    Repository Created

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,10 +1,13 @@
+using Persistence;
 using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
